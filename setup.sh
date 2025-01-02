f75346395419dd6ae040dace3164750210573ac2
Jianzu Guan <git@jzg.fastmail.com>
2025-01-02 09:20:20 +0000

    add lix installer

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix \
+  | sh -s -- install --no-confirm
+
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 
 nix-channel --update
