0133b5bc5c4f2417b532f20c5652f5a4ee3ca777
Jianzu Guan <git@jzg.fastmail.com>
2025-01-08 12:20:52 +0000

    clean empty line

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    
+
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-        
+
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
