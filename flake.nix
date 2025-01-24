ce93c26b59a7392af4e3d0e8b80642a6e75d9035
Jianzu Guan <git@jzg.fastmail.com>
2025-01-24 11:04:03 +0000

    add custom Finder and Trackpad settings; update cspell dictionary

--- flake.nix
+++ flake.nix
@@ -39,6 +39,45 @@
         finder.FXPreferredViewStyle = "clmv";
         finder.ShowPathbar = true;
         finder.ShowStatusBar = true;
+
+        # customize trackpad
+        trackpad = {
+          # tap - 轻触触摸板, click - 点击触摸板
+          Clicking = true;  # enable tap to click(轻触触摸板相当于点击)
+          TrackpadRightClick = true;  # enable two finger right click
+          # TrackpadThreeFingerDrag = true;  # enable three finger drag
+        };
+
+        # Customize settings that not supported by nix-darwin directly
+        # see the source code of this project to get more undocumented options:
+        #    https://github.com/rgcr/m-cli
+        # 
+        # All custom entries can be found by running `defaults read` command.
+        # or `defaults read xxx` to read a specific domain.
+        CustomUserPreferences = {
+          "com.apple.finder" = {
+            ShowExternalHardDrivesOnDesktop = true;
+            ShowHardDrivesOnDesktop = true;
+            ShowMountedServersOnDesktop = true;
+            ShowRemovableMediaOnDesktop = true;
+            _FXSortFoldersFirst = true;
+            # When performing a search, search the current folder by default
+            FXDefaultSearchScope = "SCcf";
+          };
+          "com.apple.desktopservices" = {
+            # Avoid creating .DS_Store files on network or USB volumes
+            DSDontWriteNetworkStores = true;
+            DSDontWriteUSBStores = true;
+          };
+          "com.apple.WindowManager" = {
+            EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
+          };
+          "com.apple.AdLib" = {
+            allowApplePersonalizedAdvertising = false;
+          };
+          # Prevent Photos from opening automatically when devices are plugged in
+          "com.apple.ImageCapture".disableHotPlug = true;
+        };
       };
 
       security.pam.enableSudoTouchIdAuth = true;
